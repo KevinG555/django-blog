6afd96d9f1f3ab984ea52a8c5acf55c3a8b94976
Ubuntu <ubuntu@ip-172-31-85-67.ec2.internal>
2019-09-25 19:07:07 +0000

    added modified files

--- posts/models.py
+++ posts/models.py
@@ -4,9 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    """
-    A single Blog post
-    """
+    
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
